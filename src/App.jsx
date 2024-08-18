2dcaf2abe30af1f7337a24ac4a0de94ae93d5df6
muhammadaccama <accamamuhammad@gmail.com>
2024-08-18 21:32:16 +0100

    nav setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import "./App.css";
-
+import Nav from "./components/common/Nav";
 
 function App() {
-
   return (
-    <>
-
-    </>
+    <main className="px-20 py-2 flex items-start justify-center">
+      <Nav />
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
-    colors: {
-      black100: "#080808",
-      white: "#FFFFFF",
-      white100: "#F7F7F7",
-      white150: "#E9E9E9",
-      white200: "#BFBBBB",
-      white300: "#F5F5F9",
-      white400: "#CBC7C8",
-      newPurple: "#682EE4",
-      longBg: "#F8D6E2",
-      mediumBg: "#E1D5FC",
-      shortBg: "#C6D1FA",
-      dailyBG: "#D1E9F6",
-      sleepBG: "#d0f0c0",
-    },
-    fontFamily: {
-      sgBold: ["SpaceGroteskBold", "mono"],
-      sgMedium: ["SpaceGroteskMediun", "mono"],
-      sgRegular: ["SpaceGroteskRegular", "mono"],
-      sgLight: ["SpaceGroteskLight", "mono"],
-    },
-    darkMode: ["selector", '[data-mode="dark"]'],
   },
   plugins: [],
-};
+}
